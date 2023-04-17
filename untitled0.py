600abe33669cda45e8ab3fafe6371beef5f4c975
siraliebi <127699992+siraliebi@users.noreply.github.com>
2023-04-18 02:47:24 +0430

    ba tavajoh be diagram class haye kham ra dorost mikonim

--- untitled0.py
+++ untitled0.py
@@ -1,23 +1,297 @@
-class Animal:
+from abc import ABC,abstractmethod
 
-    def __init__(self, age, gender, weight, height, color):
+class Animal(ABC):
+
+    def __init__(self, name:None,age:None, gender:None, weight:None, height:None, **kwargs):
+        
+        self.name = name
         self.age = age
         self.gender = gender
         self.weight = weight
         self.height = height
-        self.color = color
         
     def move(self):
-        print('moving')
+        pass
     
     def eat(self):
-        print('eating')
+        pass
         
     def breath(self):
-        print('breathing')
+        pass
         
-    def speak(self):
-        print('speaking')
+    def make_sound(self):
+        pass
         
     def drink(self):
-        print('drinking')
+        pass
+
+class Zoo(Animal):
+    
+    def __init__(self, ):
+        
+    def (self):
+        print('')    
+    
+    def (self):
+        print('')
+        
+    def (self):
+        print('') 
+        
+    def (self):
+        print('') 
+    
+    def (self):
+        print('')
+        
+class Birds(Animal):
+    
+    def __init__(self, age, gender, wings_count, feather_count):
+        self.age = age
+        self.gender = gender
+        self.wings_count = wings_count
+        self.feather_count = feather_count
+        
+    def move(self):
+        print('flies')
+        
+    def eat(self):
+        print('eats')
+        
+    def make_sound(self):
+        print('speaks')
+    
+    def mate(self):
+        print('yes or no')
+    
+    def is_wild(self):
+        print('yes or no')
+
+class Mammals(Animal):
+    
+    def __init__(self):
+        
+    def give_birth(self):
+        print('give birth')    
+    
+    def (self):
+        print('breast feed')
+        
+    def (self):
+        print('') 
+        
+    def (self):
+        print('') 
+    
+    def (self):
+        print('') 
+        
+class Reptiles(Animal):
+    
+    def __init__(self, ):
+        
+    def (self):
+        print('')    
+    
+    def (self):
+        print('')
+        
+    def (self):
+        print('') 
+        
+    def (self):
+        print('') 
+    
+    def (self):
+        print('') 
+        
+class Amphibians(Animal):
+    
+    def __init__(self, ):
+        
+    def (self):
+        print('')    
+    
+    def (self):
+        print('')
+        
+    def (self):
+        print('') 
+        
+    def (self):
+        print('') 
+    
+    def (self):
+        print('')
+        
+class Fish(Animal):
+    
+    def __init__(self, ):
+        
+    def (self):
+        print('')    
+    
+    def (self):
+        print('')
+        
+    def (self):
+        print('') 
+        
+    def (self):
+        print('') 
+    
+    def (self):
+        print('')
+        
+class Eagle(Birds):
+    
+    def __init__(self, ):
+        
+    def (self):
+        print('')    
+    
+    def (self):
+        print('')
+        
+    def (self):
+        print('') 
+        
+    def (self):
+        print('') 
+    
+    def (self):
+        print('')
+        
+class Pigeon(Birds):
+    
+    def __init__(self, ):
+        
+    def (self):
+        print('')    
+    
+    def (self):
+        print('')
+        
+    def (self):
+        print('') 
+        
+    def (self):
+        print('') 
+    
+    def (self):
+        print('')
+        
+class Human(Mammals):
+    
+    def __init__(self, ):
+        
+    def (self):
+        print('')    
+    
+    def (self):
+        print('')
+        
+    def (self):
+        print('') 
+        
+    def (self):
+        print('') 
+    
+    def (self):
+        print('')
+        
+class Snake(Reptiles):
+    
+    def __init__(self, ):
+        
+    def (self):
+        print('')    
+    
+    def (self):
+        print('')
+        
+    def (self):
+        print('') 
+        
+    def (self):
+        print('') 
+    
+    def (self):
+        print('')
+        
+class Turtle(Reptiles):
+    
+    def __init__(self, ):
+        
+    def (self):
+        print('')    
+    
+    def (self):
+        print('')
+        
+    def (self):
+        print('') 
+        
+    def (self):
+        print('') 
+    
+    def (self):
+        print('')
+    
+class Frog(Amphibians):
+    
+    def __init__(self, ):
+        
+    def (self):
+        print('')    
+    
+    def (self):
+        print('')
+        
+    def (self):
+        print('') 
+        
+    def (self):
+        print('') 
+    
+    def (self):
+        print('')
+        
+class Shark(Fish):
+    
+    def __init__(self, ):
+        
+    def (self):
+        print('')    
+    
+    def (self):
+        print('')
+        
+    def (self):
+        print('') 
+        
+    def (self):
+        print('') 
+    
+    def (self):
+        print('')
+        
+class Wale(Fish):
+    
+    def __init__(self, ):
+        
+    def (self):
+        print('')    
+    
+    def (self):
+        print('')
+        
+    def (self):
+        print('') 
+        
+    def (self):
+        print('') 
+    
+    def (self):
+        print('')
+    
